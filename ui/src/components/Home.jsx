bea6c7c8023c9a8785e7d22f16a068655bbd1d52
Mehri Amin <mehriamin5@gmail.com>
2020-03-18 21:41:32 +1100

    logout function working

--- ui/src/components/Home.jsx
+++ ui/src/components/Home.jsx
@@ -1,16 +1,54 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Button, Grid } from "@material-ui/core";
 import { AuthUserContext, withAuthorization } from "./Session";
+const useStyles = makeStyles(theme => ({
+  container: {
+    height: "10em",
+    justifyContent: "space-evenly"
+  },
+  button: {
+    border: "1px solid rgba(255,255,255,0.20)",
+    outline: "none",
+    transition: "box-shadow 399ms ease-in-out",
+    borderRadius: "12px",
+    width: "15em",
+    height: "3em",
+    boxShadow: "5px 5px 8px #e3e3e3,-5px -5px 8px #ffffff",
+    backgroundColor: "#fff",
+    "&:hover": {
+      boxShadow: "inset 5px 5px 8px #e3e3e3, inset -5px -5px 8px #ffffff",
+      color: "#2643e9",
+      backgroundColor: "#fff"
+    }
+  }
+}));
+const Home = ({ firebase, history }) => {
+  const classes = useStyles();
 
-const Home = ({ firebase }) => {
-  const currentUser = firebase.getCurrentUser();
+  const onLogout = event => {
+    firebase.doSignOut()
+  }
+
   return (
     <AuthUserContext.Consumer>
-      {authUser => (
-        <Typography>
-          Hello {currentUser ? currentUser.displayName : "World"}!
-        </Typography>
-      )}
+      {authUser => {
+        return (
+          <Grid
+            container
+            direction="column"
+            className={classes.container}
+            alignContent="center"
+          >
+            <Grid item>
+              <Typography>Hello {authUser.displayName || "World"}!</Typography>
+            </Grid>
+            <Grid item>
+              <Button onClick={onLogout} className={classes.button}>Logout</Button>
+            </Grid>
+          </Grid>
+        );
+      }}
     </AuthUserContext.Consumer>
   );
 };
