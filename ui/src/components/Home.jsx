4ae7615589b99c3c7297ba21511848553335c499
Mehri Amin <mehriamin5@gmail.com>
2020-03-18 21:24:35 +1100

    Check is user is signed in when navigating to home page, if not redirect to login page

--- ui/src/components/Home.jsx
+++ ui/src/components/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Typography, useRadioGroup } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { AuthUserContext, withAuthorization } from "./Session";
 
 const Home = ({ firebase }) => {
   const currentUser = firebase.getCurrentUser();
   return (
-    <Typography>
-      Hello {currentUser ? currentUser.displayName : "World"}!
-    </Typography>
+    <AuthUserContext.Consumer>
+      {authUser => (
+        <Typography>
+          Hello {currentUser ? currentUser.displayName : "World"}!
+        </Typography>
+      )}
+    </AuthUserContext.Consumer>
   );
 };
 
-export default Home;
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(Home);
