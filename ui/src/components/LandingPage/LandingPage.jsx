7caaa16b3838e82afe333ab7db626cb2f38c17ed
Mehri Amin <mehriamin5@gmail.com>
2020-03-29 00:40:39 +1100

    fix padding and page section sizing

--- ui/src/components/LandingPage/LandingPage.jsx
+++ ui/src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { Divider } from "@material-ui/core";
 import Hero from "./Hero";
 import AboutUs from "./AboutUs";
 
@@ -9,7 +10,16 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     flexGrow: 1,
     overflowY: "auto",
-    marginTop: "64px"
+    marginTop: "64px",
+    paddingLeft: "10em",
+    paddingRight: "10em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "2em",
+      paddingRight: "2em"
+    }
+  },
+  divider: {
+    border: "0.5px solid #2643e9"
   }
 }));
 
@@ -17,6 +27,8 @@ const LandingPage = () => {
   const classes = useStyles();
   return (
     <div className={classes.page}>
+      <Hero />
+      <Divider className={classes.divider} />
       <Hero />
     </div>
   );
