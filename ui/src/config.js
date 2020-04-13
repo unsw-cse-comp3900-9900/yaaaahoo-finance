f72929303a6e8d3ff704bc69c234744ea3669ab8
Mehri Amin <mehriamin5@gmail.com>
2020-04-13 18:19:25 +1000

    pushing config tokens

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,14 +1,14 @@
 export const config = {
-    "newsApiToken": "",
-    "alphaVantageApiToken" : "",
-    "worldTradingApiToken" : "",
-    "iexCloudApiToken" : "",
-    "apiKey": "",
-    "authDomain": "",
-    "databaseURL": "",
-    "projectId": "",
-    "storageBucket": "",
-    "messagingSenderId": "",
-    "appId": "",
-    "measurementId": ""
+    "newsApiToken": "31491f5ac1a648249ae25bf223465e1a",
+    "alphaVantageApiToken" : "DQWJXC9BHJJG7N4R",
+    "worldTradingApiToken" : "QdMKThqsC6tnqA7SD4aCGAtblOr1eY7jTPVTmTny0VKbBDGKbdaGpSmVqub5",
+    "iexCloudApiToken" : "pk_8119099a08884a29b474efa51a16eb50",
+    "apiKey": "AIzaSyAQe81lKS4ZELWIBPDaFqI9RSosAzlgpaI",
+    "authDomain": "aaaa-finance.firebaseapp.com",
+    "databaseURL": "https://aaaa-finance.firebaseio.com",
+    "projectId": "aaaa-finance",
+    "storageBucket": "aaaa-finance.appspot.com",
+    "messagingSenderId": "1075900888174",
+    "appId": "1:1075900888174:web:9890fafcd1071a1502227f",
+    "measurementId": "G-MVFVD7BDXP"
 };
