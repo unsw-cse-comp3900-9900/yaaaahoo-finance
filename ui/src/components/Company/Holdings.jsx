4f62dcc17a741082a99dfd6672be088c699ab7e8
Mehri Amin <mehriamin5@gmail.com>
2020-04-20 21:25:24 +1000

    fix merge issue

--- ui/src/components/Company/Holdings.jsx
+++ ui/src/components/Company/Holdings.jsx
@@ -3,54 +3,43 @@ import MaterialTable from "material-table";
 import { Paper } from "@material-ui/core";
 import TableContainer from "@material-ui/core/TableContainer";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
-import AddCurrentHoldingModal from "./AddCurrentHoldingModal";
 
 function createData(portfolios, companyData) {
-    const symbol = companyData.symbol;
-    const rows = [];
-    portfolios.forEach((portfolio) => {
-      let portfolioRecorded = false;
-      if (portfolio.holdings) {
-          for (let holding of Object.values(portfolio.holdings)) {
-              if (holding.symbol === symbol) {
-                  const totalGain = ((companyData.latestPrice - holding.costPerUnit) * holding.numberOfUnits)
-                      .toLocaleString(navigator.language, {minimumFractionDigits: 2});
-                  const totalPerc = ((companyData.latestPrice - holding.costPerUnit) / holding.costPerUnit * 100)
-                      .toLocaleString(navigator.language, {minimumFractionDigits: 2});
-                  const daysGain = (companyData.change * holding.numberOfUnits)
-                      .toLocaleString(navigator.language, {minimumFractionDigits: 2});
-                  console.log("Change: ", companyData.change);
-                  console.log("Latest price: ", companyData.latestPrice);
-                  console.log("Previous Close: ", companyData.previousClose);
-                  console.log("close: ", companyData.close);
-                  const daysPerc = (companyData.changePercent)
-                      .toLocaleString(navigator.language, {minimumFractionDigits: 2});
-                  rows.push({
-                      portfolio: portfolio.name,
-                      portfolioId: portfolio.id,
-                      numberOfUnits: holding.numberOfUnits,
-                      daysGain,
-                      daysPerc,
-                      totalGain,
-                      totalPerc,
-                  });
-                  portfolioRecorded = true;
-                  break;
-              }
-          }
-      }
-      if (portfolioRecorded === false) {
-        rows.push({
-          portfolio: portfolio.name,
-          portfolioId: portfolio.id,
-          numberOfUnits: holding.numberOfUnits,
-          daysGain,
-          daysPerc,
-          totalGain,
-          totalPerc,
-        });
-        portfolioRecorded = true;
-        break;
+  const symbol = companyData.symbol;
+  const rows = [];
+  portfolios.forEach((portfolio) => {
+    let portfolioRecorded = false;
+    if (portfolio.holdings) {
+      for (let holding of Object.values(portfolio.holdings)) {
+        if (holding.symbol === symbol) {
+          const totalGain = (
+            (companyData.latestPrice - holding.costPerUnit) *
+            holding.numberOfUnits
+          ).toLocaleString(navigator.language, { minimumFractionDigits: 2 });
+          const totalPerc = (
+            ((companyData.latestPrice - holding.costPerUnit) /
+              holding.costPerUnit) *
+            100
+          ).toLocaleString(navigator.language, { minimumFractionDigits: 2 });
+          const daysGain = (
+            companyData.change * holding.numberOfUnits
+          ).toLocaleString(navigator.language, { minimumFractionDigits: 2 });
+          const daysPerc = companyData.changePercent.toLocaleString(
+            navigator.language,
+            { minimumFractionDigits: 2 }
+          );
+          rows.push({
+            portfolio: portfolio.name,
+            portfolioId: portfolio.id,
+            numberOfUnits: holding.numberOfUnits,
+            daysGain,
+            daysPerc,
+            totalGain,
+            totalPerc,
+          });
+          portfolioRecorded = true;
+          break;
+        }
       }
     }
     if (portfolioRecorded === false) {
