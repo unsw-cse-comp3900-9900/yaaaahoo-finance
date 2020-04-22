9c9b579663cbc5e73fc91bdb396971fa1e5b519f
Mehri Amin <mehriamin5@gmail.com>
2020-04-23 00:16:10 +1000

    remove infinite loop on requesting news data

--- ui/src/components/TopNews/TopNews.jsx
+++ ui/src/components/TopNews/TopNews.jsx
@@ -124,7 +124,7 @@ const TopNews = ({ title, subtitle, titleColor, companies }) => {
         cancelToken.current.cancel("Component unmounted");
       }
     };
-  }, [companies, newsData]);
+  }, [companies]);
 
   return (
     <div className={classes.Page}>
