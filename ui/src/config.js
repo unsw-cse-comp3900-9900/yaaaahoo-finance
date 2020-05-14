bbdf5b0c7b1bf7fddb24d3c8b7c31a05b0398e57
Mehri Amin <z5113067@student.unsw.edu.au>
2020-05-14 18:33:46 +1000

    tokens removed

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,15 +1,3 @@
 export const config = {
-  newsApiToken: "fe80b624add743fabc5f8043669dddc7",
-  alphaVantageApiToken: "DQWJXC9BHJJG7N4R",
-  worldTradingApiToken:
-    "QdMKThqsC6tnqA7SD4aCGAtblOr1eY7jTPVTmTny0VKbBDGKbdaGpSmVqub5",
-  iexCloudApiToken: "pk_2b3f608dd9a64c28b7f8760396f5bc57 ",
-  apiKey: "AIzaSyAQe81lKS4ZELWIBPDaFqI9RSosAzlgpaI",
-  authDomain: "aaaa-finance.firebaseapp.com",
-  databaseURL: "https://aaaa-finance.firebaseio.com",
-  projectId: "aaaa-finance",
-  storageBucket: "aaaa-finance.appspot.com",
-  messagingSenderId: "1075900888174",
-  appId: "1:1075900888174:web:9890fafcd1071a1502227f",
-  measurementId: "G-MVFVD7BDXP",
+
 };
